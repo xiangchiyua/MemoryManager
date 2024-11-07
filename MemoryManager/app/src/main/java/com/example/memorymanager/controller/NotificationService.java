bb643a3b772273dadc5bc97337c63423f349eb52
TC257639365 <257639365@qq.com>
2024-11-07 14:56:06 +0800

    TC
    Observer Mode added (NotificationServise class)

--- MemoryManager/app/src/main/java/com/example/memorymanager/controller/NotificationService.java
+++ MemoryManager/app/src/main/java/com/example/memorymanager/controller/NotificationService.java
@@ -1,4 +1,46 @@
 package com.example.memorymanager.controller;
 
-public class NotificationService {
+import com.example.memorymanager.handle.Event;
+import com.example.memorymanager.handle.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationService implements Runnable {
+    private NotificationService(){}
+
+    private static NotificationService service=new NotificationService();
+
+    private List<Item>notifiedEventItemList=new ArrayList<>();
+
+    static {
+        service=new NotificationService();
+    }
+
+    public static NotificationService getService(){
+        return service;
+    }
+
+    public void updateNotifiedEventList(Event event){
+        if(event==null)
+            return; //此处可以throw一个Exception
+        Item item;
+        boolean find=false;
+        for (Item i:this.notifiedEventItemList) {
+            if(i!=null && i.getId()==event.getItem().getId()){
+                find=true;
+                item=event.getItem();
+                break;
+            }
+        }
+        if(!find && event.getItem()!=null)
+            notifiedEventItemList.add(event.getItem());
+    }
+
+    @Override
+    public void run() {
+        for (Item item:notifiedEventItemList) {
+            //item.SendNotification();
+        }
+    }
 }
